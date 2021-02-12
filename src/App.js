751dc4de86a1405772fc8af76e2c5b1a034e3978
Nestor <thenestorperez@gmail.com>
2021-02-12 11:05:05 -0400

    examples to present pm

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Example0 from './Example0';
+import Example1 from './Example1';
+// import Example2 from './Example2';
+
+function App() {
+	return (
+		<div className="App">
+			<Example1 />
+		</div>
+	);
+}
+
+export default App;
